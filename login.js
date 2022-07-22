2c77120659ddfdca83ba9d8e03e940a07f7fef8a
mariananieton <rm94141@fiap.com.br>
2022-07-22 11:26:35 -0300

    Adicionando o style.css e express.js na aplicação

--- login.js
+++ login.js
@@ -1,4 +1,8 @@
 const mysql = require("mysql");
+const express = require("express");
+
+const app = express();
+app.use("/assets",express.static("assets"));
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -12,3 +16,12 @@ connection.connect(function(error){
     if (error) throw error 
     else console.log("Conectado ao Banco de Dados com sucesso!")
 })
+
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/index.html");
+})
+
+
+// setando app port
+app.listen(4500);
